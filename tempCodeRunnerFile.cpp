0dd04649b058d90885762fca3d7c03f712b07351
ark10806 <ark10806@gmail.com>
2021-05-16 20:49:09 +0900

    To Raspi *queue size

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,9 @@
-queue<string> command_line;
+0; i<tot_comm.size(); i++){
+            string tmp = tot_comm.front(); tot_comm.pop();
+            for(int j=0; j<tmp.size(); j++){
+                if(tmp[j]==' '){
+                    q.push(tmp.substr(j, j-iter));
+                    iter = j+1;
+                }
+            }
+        }
